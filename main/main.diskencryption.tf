f74060d0fc03c2464a3b870f2654a79babe31f98
Sushil Gupta <v-sushigupta@microsoft.com>
2026-02-08 16:36:17 +0530

    purge protection disabled for testing keyvault

--- main/main.diskencryption.tf
+++ main/main.diskencryption.tf
@@ -13,7 +13,8 @@ resource "azurerm_key_vault" "kv" {
   tenant_id                  = data.azurerm_client_config.current.tenant_id
   sku_name                   = "standard"
   soft_delete_retention_days = 90
-  purge_protection_enabled   = true
+  purge_protection_enabled   = false # Set to true if you want to prevent accidental deletion of the Key Vault and its contents
+  
 
   # Enable Azure services to access the vault
   enabled_for_disk_encryption = true
